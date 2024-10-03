584d83c7133844084cc6fe05abe5b0a6fc7361f8
MAC 2016 <sobanatban4@gmail.com>
2024-10-03 20:07:41 +0500

    list.js

---
+++ backend/routes/list.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const List = require("../models/list");
+
+
+//Create
+router.post("/addTask", async (req, res) => {
+    try {
+        const { title, body, email } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const list = new List({ title, body, user: existingUser });
+            await list.save().then(() => res.status(200).json({ list }));
+            existingUser.list.push(list);
+            existingUser.save();
+        }
+    } catch (error) {
+        console.log(error);
+
+    }
+});
+
+//Update
+
+router.put("/updateTask/:id", async (req, res) => {
+    try {
+        const { title, body, email } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const list = await List.findByIdAndUpdate(req.params.id, { title, body })
+            list.save().then(() => res.status(200).json({ message: "Task Updated" }));
+        }
+    } catch (error) {
+        console.log(error);
+
+    }
+});
+module.exports = router;
