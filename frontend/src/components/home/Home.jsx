c9be84f42a3c7a7f84b52d41b35212bc24fd2dd9
MAC 2016 <sobanatban4@gmail.com>
2024-10-12 18:35:54 +0500

    added home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import "./Home.css"
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='home d-flex justify-content-center align-items-center'>
+      <div className="container">lorem15
+      </div>
     </div>
   )
 }
