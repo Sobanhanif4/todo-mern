4877aed21fdeada1d7f647305d05f72943090d45
MAC 2016 <sobanatban4@gmail.com>
2024-10-23 14:12:59 +0500

    added heading comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/footer/Footer'
 import About from './components/about/About'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Signup from './components/signup/Signup'
+import Signin from './components/signup/Signin'
 const App = () => {
   return (
     <div>
@@ -15,7 +16,7 @@ const App = () => {
           <Route exact  path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/signup' element={<Signup />} />
-          <Route path='/signin' element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
         </Routes>
       </Router>
       <Footer />
--- frontend/src/components/signup/Signin.jsx
+++ frontend/src/components/signup/Signin.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import HeadingComp from "./HeadingComp"
-import "./signup.css"
-const Signup = () => {
+
+const Signin = () => {
   return (
-    <div className="signup">
+    <div>
+      <div className="signup">
       <div className="container">
         <div className="row">
+        <div className="col-lg-4 col-left d-flex justify-content-center column align-items-center">
+            <HeadingComp first="Sign" second="In"/>
+          </div>
           <div className="col-lg-8 d-flex justify-content-center column align-items-center">
             <div className="d-flex flex-column w-100 p-5">
               <input
@@ -12,26 +17,21 @@ const Signup = () => {
                 name="email"
                 className="p-2 my-3 input-signup"
                 placeholder="Enter Your Name" />
-              <input
-                type="username"
-                name="username"
-                className="p-2 my-3 input-signup"
-                placeholder="Enter Your Username" />
+              
               <input
                 type="password"
                 name="password"
                 className="p-2 my-3 input-signup"
                 placeholder="Enter Your Password" />
-              <button className="btn-signup p-2">Sign Up</button>
+              <button className="btn-signup p-2">Sign In</button>
             </div>
           </div>
-          <div className="col-lg-4 col-left d-flex justify-content-center column align-items-center">
-            <HeadingComp first="Sign" second="Up" />
-          </div>
+          
         </div>
       </div>
+    </div>
     </div>
   )
 }
 
-export default Signup
+export default Signin
